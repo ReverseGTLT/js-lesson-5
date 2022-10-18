de0484d724d885ec0084bfb7920ef72a8cdfc637
ReverseGTLT <dzewrpifzc@gmail.com>
2022-10-18 21:21:57 +0300

    Upd error download to github

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 let numOrStr = prompt('input number or string');
-console.log(numOrStr)
-if(numOrStr === null) {
-  console.log('ви скасували')
-} else if( numOrStr.trim() === '' ) {
-  console.log('Empty String');
-} else if ( isNaN( +numOrStr ) ) {
-  console.log(' number is Ba_NaN')
-} else {
-  console.log('OK!')
+console.log(numOrStr);
+switch (numOrStr && numOrStr.trim() && isNaN(+numOrStr)) {
+  case null:
+    console.log('вы отменили');
+    break;
+  case '':
+    console.log('Empty String');
+    break;
+  case true:
+    console.log(' number is Ba_NaN');
+    break;
+  default:
+    console.log('OK!');
 }
